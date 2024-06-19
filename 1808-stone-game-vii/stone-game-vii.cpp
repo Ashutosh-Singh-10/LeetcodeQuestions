6951162fae417ef9322aabe0ffe66eb38b917f25
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2024-06-19 16:09:40 +0530

    Time: 117 ms (72.57%) | Memory: 87.1 MB (31.95%) - LeetSync

--- 1808-stone-game-vii/stone-game-vii.cpp
+++ 1808-stone-game-vii/stone-game-vii.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
-    int fun(vector<int> &piles,int i,int j,vector<int> &pre,vector<vector<int>> &dp)
-    {
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        if(j==i) return 0;
-        int ans=pre[j+1]-pre[i];
-        ans=max(pre[j+1]-pre[i+1]-fun(piles,i+1,j,pre,dp),pre[j]-pre[i]-fun(piles,i,j-1,pre,dp));
-        dp[i][j]=ans;
-        return ans;
     
-    }
     int stoneGameVII(vector<int>& piles) {
         int n=piles.size();
         vector<int> pre={0};
-        vector<vector<int>> dp(n,vector<int> (n,-1));
+        vector<vector<int>> dp(n,vector<int> (n,0));
         for(auto &i:piles) pre.push_back(pre.back()+i);
-        return fun(piles,0,n-1,pre,dp);
+     
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                dp[i][j]=max(
+                    pre[n-i-1+j] 
+                -pre[j]   
+                -dp[i+1][j]
+                ,
+                            pre[n-i+j] 
+                            -pre[j+1]  
+                             -dp[i+1][j+1] 
+                            );
+                
 
+
+            }
+
+        }
+        // for(auto &i:dp)
+        // {
+        //     for(auto &j:i) cout<<j<< " ";cout<<endl;
+
+        // }
+        return dp[0][0];
         
+
+         
     }
 };
