ffe40a083b0be170fac50d6cd07960877b8cdf1c
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2024-07-02 09:49:42 +0530

    Time: 393 ms (73.39%) | Memory: 174.2 MB (81.07%) - LeetSync

---
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -0,0 +1,35 @@
+long long dfs(vector<vector<int>> & gr,long long & ans,int &seats,int cur,int prev)
+{
+    long long sum=1;
+    for(auto &i:gr[cur])
+    {
+        if(i!=prev)
+        {
+            sum+=dfs(gr,ans,seats,i,cur);
+
+        }
+        
+    }
+    if(cur!=0)
+    ans+=(sum/seats)+(sum%seats?1:0);
+    return sum;
+
+}
+class Solution {
+public:
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n=roads.size()+1;
+        vector<vector<int>> gr(n);
+        for(auto &i:roads) 
+        {
+            gr[i[0]].push_back(i[1]);
+            gr[i[1]].push_back(i[0]);
+        }
+        long long ans=0;
+        dfs(gr,ans,seats,0,-1);
+        return ans;
+
+
+        
+    }
+};
