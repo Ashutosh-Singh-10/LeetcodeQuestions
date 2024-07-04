cba28e5ff7ec3bd8bf05e8504370da440f23c78d
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2024-07-04 10:54:12 +0530

    Time: 14 ms (45.54%) | Memory: 16.7 MB (51.04%) - LeetSync

---
+++ 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
@@ -0,0 +1,44 @@
+int fun(vector<int> &vec,int k)
+{   
+    int n=vec.size();
+    vector<vector<int>>dp(n+2,vector<int>(k+1));
+    int ans=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<k;j++)
+        {
+            dp[i+2][j+1]=max(dp[i][j+1],dp[i][j]+vec[i]);
+            dp[i+2][j+1]=max(dp[i+2][j+1],dp[i+1][j+1]);
+            ans=max({dp[i+2][j+1],ans});
+            
+        }
+    }
+    // for(auto &i:vec) cout<<i<<" ";cout<<endl;;
+    // for(auto &i:dp)
+    // {
+    //     for(auto &j:i) cout<<j<<" ";
+    //     cout<<endl;
+    // }
+    // cout<<endl;
+    return ans;
+    
+
+
+}
+class Solution {
+public:
+    int maxSizeSlices(vector<int>& slices) {
+        int x=slices.back();
+        int k=slices.size()/3;
+        slices.pop_back();
+        vector<int> vec;
+        for(int i=1;i<slices.size();i++) vec.push_back(slices[i]);
+        vec.push_back(x);
+        int ans=fun(slices,k);
+        
+        return max(ans,fun(vec,k));
+
+        
+        
+    }
+};
