5641943a6d083c5a7e39cb5b3d27d6c2e4546b6b
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2024-07-06 22:18:16 +0530

    Time: 120 ms (25.00%) | Memory: 76 MB (75.00%) - LeetSync

---
+++ 3264-maximum-points-after-enemy-battles/maximum-points-after-enemy-battles.cpp
@@ -0,0 +1,39 @@
+	#include <bits/stdc++.h>
+	using namespace std;
+	using std::cout;
+	using std::cin;
+	#define endl '\n'    
+	#define ll long long
+	#define loop(i,a,b) for(long long i=a;i<b;i++)
+	#define roop(i,a,b) for(long long i=a-1;i>=b;i--)
+	#define all(p) p.begin(),p.end()
+	#define pb(p) push_back(p);
+	long long pop_count(long long a){int res=0;while(a){res+=(a&1),a>>=1;}return res;}
+	template<class T> using _pq = priority_queue<T>;
+	template<class T> using _pqr = priority_queue<T, vector<T>, greater<T>>;
+	template<class T> void So(vector<T> &v) {sort(v.begin(),v.end());}
+	template<class T> void Sore(vector<T> &v) {sort(v.begin(),v.end(),[](T x,T y){return x>y;});}
+	#include <ext/pb_ds/assoc_container.hpp>
+    #include <ext/pb_ds/tree_policy.hpp>
+    using namespace __gnu_pbds;
+    #include<iostream>
+    template <class T> using OrderSet = tree<T, null_type, 
+    less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+    ll lcm(ll a, ll b){return (a/__gcd(a,b)*b);}
+    const ll INF= LONG_LONG_MAX;
+class Solution {
+public:
+    long long maximumPoints(vector<int>& vec, int cur) {
+		So(vec);
+        if(vec[0]>cur) return 0;
+		
+		ll sum=accumulate(all(vec),0ll)-vec[0]+cur;
+		ll ans=sum/vec[0];
+		
+		
+		return ans;
+		
+		
+        
+    }
+};
