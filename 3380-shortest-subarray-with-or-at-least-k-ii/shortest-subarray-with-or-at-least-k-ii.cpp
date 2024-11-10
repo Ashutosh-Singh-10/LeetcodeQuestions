c4c160ac014032398d9acaff5ae9cb125824fd6a
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2024-11-10 13:57:17 +0530

    Time: 203 ms (27.47%) | Memory: 88.4 MB (44.57%) - LeetSync

---
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        vector<int> vec(35,0);
+        if(k==0) return 1;
+        int n=nums.size(),idx=0;
+        int ans=n+1,val=0;
+        for(int i=0;i<n;i++)
+        {
+            val|=nums[i];
+            for(int j=0;j<=30;j++)
+            {
+                if(nums[i]&(1<<j)) vec[j]++;
+            }
+            while(val>=k)
+            {
+                cout<<idx;
+                ans=min(ans,i-idx+1);
+                
+                for(int j=0;j<=30;j++)
+                {
+                    if(nums[idx]&(1<<j))
+                    {
+
+                        vec[j]--;
+                        if(vec[j]==0)
+                        val^=(1<<j);
+                    } 
+                }
+                idx++;
+            }
+
+
+        }
+        if(ans==n+1) return -1;
+        return max(ans,1);
+        
+    }
+};
