88cc4943ac561e74deab4c39580fd17319b3829c
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2024-11-22 22:10:13 +0530

    Time: 970 ms (79.92%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -2,7 +2,7 @@ select a.name,b.bonus
 from
  employee as a
   left join 
-bonus as b on a.empid=b.empid having not bonus>=1000 or bonus is null;
+bonus as b on a.empid=b.empid having bonus<1000 or bonus is null;
 
 
 
