f973a6cdaa1ae0318c64d056547ef83a9c44d293
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2025-01-21 12:18:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (21.86%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -20,15 +20,12 @@ public:
                 cnt=1;
                 prev=ch;
             }
-
         }
-
-                ans.push_back(prev);
-                if(cnt>1)
-                {
-                    ans+=to_string(cnt);
-                }
-                // cout<<ans<<endl;
+        ans.push_back(prev);
+        if(cnt>1)
+        {
+            ans+=to_string(cnt);
+        }
         for(int i=0;i<ans.length();i++) 
         {
             chars[i]=ans[i];
