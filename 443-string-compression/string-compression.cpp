6838b42f72e2718ecfb50ace0813020ffa1bbd24
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2025-01-21 12:17:48 +0530

    Time: 2 ms (17.85%) | Memory: 13.9 MB (21.86%) - LeetSync

---
+++ 443-string-compression/string-compression.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        string ans;
+        int prev=chars[0];
+        int cnt=0;
+        for(auto &ch:chars)
+        {
+            if(ch==prev) 
+            {
+                cnt++;
+            }
+            else 
+            {
+                ans.push_back(prev);
+                if(cnt>1)
+                {
+                    ans+=to_string(cnt);
+                }
+                cnt=1;
+                prev=ch;
+            }
+
+        }
+
+                ans.push_back(prev);
+                if(cnt>1)
+                {
+                    ans+=to_string(cnt);
+                }
+                // cout<<ans<<endl;
+        for(int i=0;i<ans.length();i++) 
+        {
+            chars[i]=ans[i];
+        }
+        return ans.length();
+        
+    }
+};
