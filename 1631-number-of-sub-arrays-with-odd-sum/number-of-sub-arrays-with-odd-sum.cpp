6f528f910af39e7ba2ee4c0667d93b32f746e6ae
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2025-02-25 11:07:36 +0530

    Time: 210 ms (5.23%) | Memory: 205 MB (5.77%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -1,12 +1,13 @@
+#define ll long long 
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        int n=arr.size();
-        vector<vector<int> > dp(n+1,vector<int>(3,0));
-        int ans=0,mod=1e9+7;
-        for(int i=1;i<=n;i++)
+        ll n=arr.size();
+        vector<vector<ll> > dp(n+1,vector<ll>(3,0));
+        ll ans=0,mod=1e9+7;
+        for(ll i=1;i<=n;i++)
         {
-            int el=arr[i-1];
+            ll el=arr[i-1];
             if(el%2)
             {
                 dp[i][1]=dp[i-1][2];
@@ -21,6 +22,7 @@ public:
             }
             ans=(ans+dp[i][1])%mod;
         }
+        cout<<ans<<endl;
         return ans;
         
         
