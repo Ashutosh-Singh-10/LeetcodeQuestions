7066cde6ca610c7c03da89fd402e5d5da58b9835
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2025-04-07 12:28:37 +0530

    Time: 21 ms (98.25%) | Memory: 14.8 MB (82.14%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;    
+#define ll long long
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define longloop(i,a,b) for(long long i=a;i<b;i++)
+#define roop(i,a,b) for(int i=a-1;i>=b;i--)
+#define all(p) p.begin(),p.end()
+#define pb(p) push_back(p);
+int pop_count(long long a){int res=0;while(a){res+=(a&1),a>>=1;}return res;}
+template<class T> using _pq = priority_queue<T>;
+template<class T> using _pqr = priority_queue<T, vector<T>, greater<T>>;
+template<class T> void So(vector<T> &v) {sort(v.begin(),v.end());}
+template<class T> void Sore(vector<T> &v) {sort(v.begin(),v.end(),[](T x,T y){return x>y;});}
+
+template<class T> void vec_out(vector<T> &p,int ty=0){
+if(ty==2){cout<<'{';for(int i=0;i<(int)p.size();i++){if(i){cout<<",";}cout<<'"'<<p[i]<<'"';}cout<<"}\n";}
+else{if(ty==1){cout<<p.size()<<"\n";}for(int i=0;i<(int)(p.size());i++){if(i) cout<<" ";cout<<p[i];}cout<<"\n";}}
+
+template<class T> bool chmin(T &a,const T &b){if(a>b){a=b;return 1;}else return 0;}
+template<class T> bool chmax(T &a,const T &b){if(a<b){a=b;return 1;}else return 0;} 
+
+template<class T> T vec_min(vector<T> &a){assert(!a.empty());T ans=a[0];for(auto &x:a) chmin(ans,x);return ans;}
+template<class T> T vec_max(vector<T> &a){assert(!a.empty());T ans=a[0];for(auto &x:a) chmax(ans,x);return ans;}
+template<class T> T vec_sum(vector<T> &a){assert(!a.empty());T ans=a[0]-a[0];for(auto &x:a) ans+=x;return ans;}
+
+template<class T>  void matcout(vector<vector<T>> &a){for(auto &i:a){ for(auto &j:i){cout<<j<<"  ";}cout<<"\n";}cout<<"\n";}
+
+template<class T> ll LB(vector<T> &v,T a){return lower_bound(v.begin(),v.end(),a)-v.begin();}
+template<class T> ll UB(vector<T> &v,T a){return upper_bound(v.begin(),v.end(),a)-v.begin();}
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int x=accumulate(all(nums),0);
+        if(x%2) return 0;
+        x/=2;
+        vector<int> vec(x+1,0);
+        vec[0]=1;
+        loop(i,0,nums.size())
+        {
+            roop(j,x+1,0)
+            {
+                if(vec[j]==1 && j+nums[i]<x+1)
+                {
+                    vec[j+nums[i]]=1;
+                }
+            }
+        }
+        // loop(i,0,x+1) cout<<vec[i]<<" ";
+        return vec[x];
+
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
