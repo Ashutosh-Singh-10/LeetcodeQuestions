024cf1d60d5c3eadcd5e3e63bc577bd6c1802fac
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2025-07-27 19:07:16 +0530

    Time: 1713 ms (12.81%) | Memory: 489.4 MB (5.01%) - LeetSync

---
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -0,0 +1,89 @@
+int mex=1e6+5;
+vector<int> vec(1e6+5,0);
+class Solution {
+public:
+    void calculate(){
+        vec[0]=vec[1]=1;
+        for(int i=2;i*i<mex;i++)
+        {
+            if(vec[i]==0)
+            {
+                for(int j=i+i;j<mex;j+=i)
+                {
+                    vec[j]=1;
+
+                }
+                
+            }
+        }
+
+    }
+    int minJumps(vector<int>& nums) {
+        if(vec[4]==0) calculate();
+        int n=nums.size();;
+        int mx=0;
+        unordered_map<int,vector<int>> data;
+        for(int i=0;i<n;i++)
+        {
+            data[nums[i]].push_back(i);
+            mx=max(mx,nums[i]);
+
+        } 
+        queue<int> qu;
+        vector<int> vis(n,0);
+        vis[0]=1;
+        qu.push(0);
+        qu.push(-1);
+        int ans=0;
+        unordered_set<int> primeVis;
+        while(qu.size())
+        {
+            if(qu.front()==-1)
+            {
+                ans++;
+                qu.push(-1);
+                qu.pop();
+                continue;
+            }
+            if(qu.front()==n-1) return ans;
+            int x=qu.front();
+            // cout<<x<<" ";
+            qu.pop();
+            
+            if(vis[x+1]==0) 
+            {
+                qu.push(x+1);
+                vis[x+1]=1;
+            }
+            if(x!=0 && vis[x-1]==0) 
+            {
+                qu.push(x-1);
+                vis[x-1]=1;
+            }
+            if(vec[nums[x]]==0 && primeVis.count(nums[x])==0)
+            {
+                primeVis.insert(nums[x]);
+                cout<<nums[x]<<" ";
+                for(int i=nums[x];i<=mx;i+=nums[x])
+                {
+                    for(auto &j:data[i])
+                    {
+                        if(vis[j]==0)
+                        {
+                            vis[j]=1;
+                            qu.push(j);
+                        }
+                        // cout<<j<< " ";
+
+                    }
+                }
+                // cout<<"}";
+            }
+        }
+        return 0;
+
+
+
+        
+    }
+};
