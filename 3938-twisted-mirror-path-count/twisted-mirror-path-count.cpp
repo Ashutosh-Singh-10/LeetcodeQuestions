6a51684d541f3ef8285332121d46112a0a435877
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2025-08-30 22:09:54 +0530

    Time: 1010 ms (40.98%) | Memory: 492.2 MB (6.43%) - LeetSync

--- 3938-twisted-mirror-path-count/twisted-mirror-path-count.cpp
+++ 3938-twisted-mirror-path-count/twisted-mirror-path-count.cpp
@@ -38,24 +38,10 @@ public:
                         dp[i][j][0]=a;
                         dp[i][j][1]=b;
                     }
-
-
-
                 }
-                
-
             }
-
         }
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=0;j<m;j++)
-        //     {
-        //         cout<<"{ "<<dp[i][j][0]<<" "<<dp[i][j][1]<<" }  ";
-
-        //     }
-        //     cout<<endl;
-        // }
+       
         return dp[n-1][m-1][0];
         
     }
