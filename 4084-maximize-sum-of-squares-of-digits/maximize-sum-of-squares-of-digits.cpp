2e43a7a4480b0e48610facdd5732a5086421cd55
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2025-10-25 23:09:21 +0530

    Time: 66 ms (9.52%) | Memory: 62 MB (9.52%) - LeetSync

---
+++ 4084-maximize-sum-of-squares-of-digits/maximize-sum-of-squares-of-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    string maxSumOfSquares(int num, int sum) {
+        int dig=sum/9;
+        if(sum%9) dig++;
+        if(dig>num) return "";
+        string ans;
+        while(ans.size()<num){
+            if(sum>9) {
+                ans.push_back('9');
+                sum-=9;
+            }
+            else  {
+                ans.push_back('0'+sum);
+                sum=0;
+            }
+
+        }
+        return ans;
+        
+    }
+}; 
