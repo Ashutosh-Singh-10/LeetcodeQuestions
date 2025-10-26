073c76f02f567d777a7ae9a383aaaae324cf308b
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2025-10-26 19:49:26 +0530

    Time: 1651 ms (11.11%) | Memory: 336.6 MB (11.11%) - LeetSync

---
+++ 4063-count-distinct-subarrays-divisible-by-k-in-sorted-array/count-distinct-subarrays-divisible-by-k-in-sorted-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    long long numGoodSubarrays(vector<int>& nums, int k,int flag=1) {
+        long long ans=0;
+        map<long long,long long> mp;
+        mp[0]=1;
+        long long mod=0;
+        for(auto &i:nums) {
+            mod=(mod+i)%k;
+            ans+=mp[mod];
+            mp[mod]++;
+        }
+        if(flag==0) return ans;
+        vector<vector<int>> vec;
+        for(auto &i:nums){
+            if(vec.size()==0) {
+                vec.push_back({i});
+            }
+            else if(i==vec.back().back()){
+                vec.back().push_back(i);
+            }
+            else vec.push_back({i});
+        }
+        for(auto &i:vec) {
+            long long x=numGoodSubarrays(i,k,0);
+            long long el=i[0];
+            if(x>=1){
+                long long lcm=(el*(long long)k)/__gcd(el,(long long)k);
+                long long sum=el*i.size();
+                long long tot=sum/lcm;
+                if(tot>1) ans+=tot-1;
+
+            }
+            
+            if(x>1) ans-=x-1;
+             cout<<x<<" ";
+        }
+        return ans;
+
+        return ans;
+        
+    }
+};
