9f714ecb0534f3456da09cf355e5b2b735e0c03d
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2025-12-25 21:40:31 +0530

    Time: 154 ms (65.96%) | Memory: 107.8 MB (65.96%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,53 @@
+        #include <bits/stdc++.h>
+        using namespace std;
+        using std::cout;
+        using std::cin;
+        #define endl '\n'    
+        #define ll long long
+        #define loop(i,a,b) for(long long i=a;i<b;i++)
+        #define roop(i,a,b) for(long long i=a-1;i>=b;i--)
+        #define all(p) p.begin(),p.end()
+        #define pb(p) push_back(p);
+        long long pop_count(long long a){int res=0;while(a){res+=(a&1),a>>=1;}return res;}
+        template<class T> using _pq = priority_queue<T>;
+        template<class T> using _pqr = priority_queue<T, vector<T>, greater<T>>;
+        template<class T> void So(vector<T> &v) {sort(v.begin(),v.end());}
+        template<class T> void Sore(vector<T> &v) {sort(v.begin(),v.end(),[](T x,T y){return x>y;});}
+
+        template<class T> void vec_out(vector<T> &p,int ty=0){
+        if(ty==2){cout<<'{';for(int i=0;i<(int)p.size();i++){if(i){cout<<",";}cout<<'"'<<p[i]<<'"';}cout<<"}\n";}
+        else{if(ty==1){cout<<p.size()<<"\n";}for(int i=0;i<(int)(p.size());i++){if(i) cout<<" ";cout<<p[i];}cout<<"\n";}}
+
+        template<class T> bool chmin(T &a,const T &b){if(a>b){a=b;return 1;}else return 0;}
+        template<class T> bool chmax(T &a,const T &b){if(a<b){a=b;return 1;}else return 0;} 
+
+        template<class T> T vec_min(vector<T> &a){assert(!a.empty());T ans=a[0];for(auto &x:a) chmin(ans,x);return ans;}
+        template<class T> T vec_max(vector<T> &a){assert(!a.empty());T ans=a[0];for(auto &x:a) chmax(ans,x);return ans;}
+        template<class T> T vec_sum(vector<T> &a){assert(!a.empty());T ans=a[0]-a[0];for(auto &x:a) ans+=x;return ans;}
+
+        template<class T>  void matcout(vector<vector<T>> &a){for(auto &i:a){ for(auto &j:i){cout<<j<<"  ";}cout<<"\n";}cout<<"\n";}
+
+        template<class T> ll LB(vector<T> &v,T a){return lower_bound(v.begin(),v.end(),a)-v.begin();}
+        template<class T> ll UB(vector<T> &v,T a){return upper_bound(v.begin(),v.end(),a)-v.begin();}
+
+template<typename T1> void printIt(T1 t1){cout<<t1<<"\n";}
+template<typename T1, typename... T2>
+void printIt(T1 t1,T2... t2) {cout<<t1<<" , "; printIt(t2...);}
+
+
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& vec, int k) {
+
+        Sore(vec);
+        ll ans=0;
+        for(int i=0;i<k;i++)
+        {
+            ans+=max((ll)0,(ll)vec[i]-i);
+
+        }
+        return ans;
+
+        
+    }
+};
