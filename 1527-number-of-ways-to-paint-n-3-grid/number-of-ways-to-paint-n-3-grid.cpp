f7c45bebc6f7c31fcad561b4e05ed4093ad00434
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2026-01-03 10:21:01 +0530

    Time: 24 ms (37.67%) | Memory: 19.5 MB (18.67%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -1,8 +1,5 @@
 class Solution {
 public:
-    int numOfWays(int n) {
-        long long mod=1e9+7,ans=0;
-        vector<vector<long long>> dp(n+1,vector<long long>(12,0));
         vector<pair<long long,long long>> vec={
             {0,4},{0,5},{0,7},{0,8},{0,9},
             {1,4},{1,6},{1,7},{1,8},
@@ -13,6 +10,9 @@ public:
             {6,8},{6,9},{6,11},
             {7,9},{7,10},{7,11}
         };
+    int numOfWays(int n) {
+        long long mod=1e9+7,ans=0;
+        vector<vector<long long>> dp(n+1,vector<long long>(12,0));
         for(int i=0;i<12;i++) dp[0][i]=1;
         for(int i=1;i<n;i++){
             for(auto &[a,b]:vec){
