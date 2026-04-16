b8dc5d7cf66b2fab0711a59d68dc1ee4b784b67a
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2026-04-17 01:15:46 +0530

    Time: 593 ms (6.03%) | Memory: 222.2 MB (20.60%) - LeetSync

---
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        int n=nums.size();
+        vector<int> vec(n,0);
+        map<int,vector<int>> mp;
+        for(int i=0;i<n;i++){
+            vec[i]=mp[nums[i]].size();
+            mp[nums[i]].push_back(i);
+        } 
+        vector<int> ans;
+        for(auto &i:queries){
+            int el=nums[i];
+            if(mp[el].size()<2){
+                ans.push_back(-1);
+                // cout<<"----"<<endl;
+                continue;
+            }
+            ans.push_back(min( 
+                {
+                abs(mp[el][vec[i]]-mp[el][
+                    (vec[i]-1+mp[el].size())%mp[el].size()
+                    ]
+                ),
+                n-abs(mp[el][vec[i]]-mp[el][
+                    (vec[i]-1+mp[el].size())%mp[el].size()
+                    ]
+                ),
+                abs(mp[el][vec[i]]-mp[el][
+                    (vec[i]+1+mp[el].size())%mp[el].size()
+                    ]
+                ),
+                n-abs(mp[el][vec[i]]-mp[el][
+                    (vec[i]+1+mp[el].size())%mp[el].size()
+                    ]
+                )
+                }
+            ));
+            // vector<int> tmp={
+            //     abs(mp[el][vec[i]]-mp[el][
+            //         (vec[i]-1+mp[el].size())%mp[el].size()
+            //         ]
+            //     ),
+            //     n-abs(mp[el][vec[i]]-mp[el][
+            //         (vec[i]-1+mp[el].size())%mp[el].size()
+            //         ]
+            //     ),
+            //     abs(mp[el][vec[i]]-mp[el][
+            //         (vec[i]+1+mp[el].size())%mp[el].size()
+            //         ]
+            //     ),
+            //     n-abs(mp[el][vec[i]]-mp[el][
+            //         (vec[i]+1+mp[el].size())%mp[el].size()
+            //         ]
+            //     )
+            //     };
+                // for(auto &i:tmp) cout<<i<<" --- ";cout<<endl;
+            // cout<<abs(mp[el][vec[i]]-mp[el][
+            //         (vec[i]-1+mp[el].size())%mp[el].size()
+            //         ]
+            //     )<<"   ---  "<<abs(mp[el][vec[i]]-mp[el][
+            //         (vec[i]+1+mp[el].size())%mp[el].size()
+            //         ]
+            //     )<<endl;
+
+
+        }
+        return ans;
+
+        
+    }
+};
