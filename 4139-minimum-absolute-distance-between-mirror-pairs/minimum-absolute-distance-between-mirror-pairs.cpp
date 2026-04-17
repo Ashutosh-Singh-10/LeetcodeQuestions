446ae2c9f6b49333efbaa6e2e160c06b3705b23e
Ashutosh Singh <83817283+Ashutosh-Singh-10@users.noreply.github.com>
2026-04-18 01:21:41 +0530

    Time: 326 ms (15.74%) | Memory: 137.3 MB (28.81%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int minMirrorPairDistance(vector<int>& nums) {
+        map<int,int> mp;
         int n=nums.size();
-        int ans=n;
-        map<string,int> mp;
-        for(int j=0;j<n;j++){
-            int i=nums[j];
-            string str=to_string(i);
-            reverse(str.begin(),str.end());
-            if(mp.count(str)){
-                ans=min(ans,j-mp[str]);
-            } 
-            reverse(str.begin(),str.end());
-            int flag=0;
-            string add;
-            while(str.back()=='0') str.pop_back();
-            // for(auto &i:str) {
-            //     if(flag==1) add.push_back(i);
-            //     else if(i!='0'){
-            //         flag=1;
-            //         add.push_back(i);
-            //     }
-            // }
-            mp[str]=j;
+        int ans=1e9;
+        for(int i=n-1;i>=0;i--) {
+            string s=to_string(nums[i]);
+            reverse(s.begin(),s.end());
+            int el=stoi(s);
+            if(mp.count(el)){
+                ans=min(ans,mp[el]-i);
+            }
+            mp[nums[i]]=i;
         }
-        // for(auto &[a,b]:mp) cout<<a<<"->"<<b<<endl;
-        return ans==n?-1:ans;
+        return ans==1e9?-1:ans;
+        
         
     }
 };
